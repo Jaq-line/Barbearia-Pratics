d18ec56d38b979db28ecbda627502c83d63d67cc
Jaq-line <104545664+Jaq-line@users.noreply.github.com>
2022-06-04 00:00:24 -0300

    arquivos do site

---
+++ code.java
@@ -0,0 +1,21 @@
+//nome do programa
+//meu primeiro programa java
+
+class meuprimeiroexerciciojava
+{
+public static void main (string entrada [] )
+{
+
+int idade = 25;
+char genero = 'f';
+double peso = 50.5;
+string nome = "jaque";
+string curso = "sistemas para internet";
+
+system.out.println ("eu sou a " + nome + "tenho " + 
+idade + "anos e tenho " + peso "kg de peso e curso " + curso;
+
+system.exit (0);
+}
+
+}
